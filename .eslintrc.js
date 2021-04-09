c56c29be9b1c71c4dcc240868c2aee43edad0715
jacky.li <jacky.li@accenture.com>
2021-04-09 11:55:21 +0800

    follow the staructure from @polkadot/common

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+const base = require('@polkadot/dev/config/eslint.cjs');
+
+module.exports = {
+  ...base,
+  ignorePatterns: [
+    '.eslintrc.js',
+    '.github/**',
+    '.vscode/**',
+    '.yarn/**',
+    '**/build/*',
+    '**/coverage/*',
+    '**/node_modules/*'
+  ],
+  parserOptions: {
+    ...base.parserOptions,
+    project: [
+      './tsconfig.json'
+    ]
+  },
+  rules: {
+    ...base.rules,
+    
+    // this seems very broken atm, false positives
+    '@typescript-eslint/unbound-method': 'off'
+  }
+};
---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+const config = require('@polkadot/dev/config/jest.cjs');
+
+module.exports = Object.assign({}, config, {
+  moduleNameMapper: {
+    
+  },
+  modulePathIgnorePatterns: [
+    
+  ],
+  transformIgnorePatterns: []
+});
