fbd721e01240c4f2764c3b2fd43153aa291fe9cd
jacky.li <jacky.li@accenture.com>
2021-04-09 23:58:15 +0800

    update

--- packages/utils/src/__tests__/index.spec.ts
+++ packages/utils/src/__tests__/index.spec.ts
@@ -1,7 +1,13 @@
-import * as _ from 'lodash';
+import {_, moment, uuid} from '../';
 
 describe('utils/index', () => {
   it('lodash verison should be correct', () => {
     expect(_.VERSION).toEqual('4.17.21');
   });
+
+  it('moment verison should be correct', () => {
+    expect(moment.version).toEqual('2.29.1');
+  });
+
+
 });
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,5 +1,13 @@
+
 import * as _ from 'lodash';
+import moment from 'moment';
+import * as uuid_pkg from 'uuid';
+
+export const uuid = (): string => {
+  return uuid_pkg.v4();
+};
 
 export {
-  _
+  _,
+  moment,
 };
